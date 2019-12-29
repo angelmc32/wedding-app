cb8f442657a2d76b71ded08feff90b4e4424f18a
angelmc32 <angel.melendez.c@gmail.com>
2019-12-28 19:54:55 -0600

    :construction: Responsive navbar component

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import bulldog from '../images/bulldog.svg'
 
 const Header = ({ siteTitle }) => (
   <nav className="uk-navbar uk-navbar-container">
@@ -8,27 +9,33 @@ const Header = ({ siteTitle }) => (
     <div className="uk-navbar-left">
 
         <ul className="uk-navbar-nav">
-            <li className="uk-active"><a href="#">Paula y Angel</a></li>
+            <li className="uk-active uk-flex uk-flex-middle">
+                <div className="uk-margin-left">
+                    <img src={bulldog} width="40px" height="40px" alt="" className="uk-img" />
+                </div>
+                <a href="#">P&A</a>
+            </li>
         </ul>
 
     </div>
 
     <div className="uk-navbar-right">
 
-        <ul className="uk-navbar-nav">
-            <li className="uk-active"><a href="#">Active</a></li>
-            <li>
-                <a href="#">Parent</a>
-                <div className="uk-navbar-dropdown">
-                    <ul className="uk-nav uk-navbar-dropdown-nav">
-                        <li className="uk-active"><a href="#">Active</a></li>
-                        <li><a href="#">Item</a></li>
-                        <li><a href="#">Item</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li><a href="#">Item</a></li>
-        </ul>
+        <label htmlFor="toggle"><span uk-icon="menu"></span></label>
+        <input type="checkbox" id="toggle"/>
+
+        <div className="menu">
+            <a href="#">Detalles</a>
+            <a href="#">Donde</a>
+            <a href="#">Contacto</a>
+        </div>
+
+        {/* <ul className="uk-navbar-nav">
+            <li className="uk-active"><a href="#">Detalles</a></li>
+            <li className="uk-active"><a href="#">Donde</a></li>
+            <li className="uk-active"><a href="#">Contacto</a></li>            
+            &#9776;
+        </ul> */}
 
     </div>
 
