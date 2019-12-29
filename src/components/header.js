a30f2003ab657c2d602ad5d3e9b0b9f4154a0234
angelmc32 <angel.melendez.c@gmail.com>
2019-12-29 01:27:33 -0600

    :construction: Improved responsive navbar component, added new image and improved toggle hamburger menu for small screens

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,32 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import bulldog from '../images/bulldog.svg'
+import bulldog from '../images/bulldogs.svg'
 
 const Header = ({ siteTitle }) => (
-  <nav className="uk-navbar uk-navbar-container">
+  <nav className="uk-navbar uk-navbar-container uk-flex uk-flex-between uk-flex-middle uk-flex-wrap">
+    
+    <ul className="uk-navbar-nav uk-height-1-1">
+        <li className="uk-active uk-flex uk-flex-middle uk-height-1-1">
+            <div className="uk-margin-left">
+                <img src={bulldog} width="64px" height="64px" alt="" className="uk-img" />
+            </div>
+        </li>
+        <li className="uk-active uk-flex uk-flex-middle uk-height-1-1">
+            <a href="#">P&A</a>
+        </li>
+    </ul>
 
-    <div className="uk-navbar-left">
+    <label htmlFor="toggle" className="uk-margin-right uk-height-1-1"><span uk-icon="menu"></span></label>
+    <input type="checkbox" id="toggle"/>
+    
+    <ul className="menu uk-navbar-nav uk-height-1-1">
+        <li className="uk-active uk-height-1-1 uk-width-1-1 uk-flex uk-flex-center uk-flex-middle"><a href="#">Detalles</a></li>
+        <li className="uk-active uk-height-1-1 uk-width-1-1 uk-flex uk-flex-center uk-flex-middle"><a href="#">Donde</a></li>
+        <li className="uk-active uk-height-1-1 uk-width-1-1 uk-flex uk-flex-center uk-flex-middle"><a href="#">Contacto</a></li>
+    </ul>
 
-        <ul className="uk-navbar-nav">
-            <li className="uk-active uk-flex uk-flex-middle">
-                <div className="uk-margin-left">
-                    <img src={bulldog} width="40px" height="40px" alt="" className="uk-img" />
-                </div>
-                <a href="#">P&A</a>
-            </li>
-        </ul>
-
-    </div>
-
-    <div className="uk-navbar-right">
-
-        <label htmlFor="toggle"><span uk-icon="menu"></span></label>
-        <input type="checkbox" id="toggle"/>
-
-        <div className="menu">
-            <a href="#">Detalles</a>
-            <a href="#">Donde</a>
-            <a href="#">Contacto</a>
-        </div>
+        
 
         {/* <ul className="uk-navbar-nav">
             <li className="uk-active"><a href="#">Detalles</a></li>
@@ -37,7 +35,11 @@ const Header = ({ siteTitle }) => (
             &#9776;
         </ul> */}
 
-    </div>
+    
+
+    
+
+    
 
 </nav>
 )
